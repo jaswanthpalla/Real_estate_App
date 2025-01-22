9f14c5f72e2489574f80b262704097ae8b1af632
jaswanthpalla <116431092+jaswanthpalla@users.noreply.github.com>
2025-01-22 18:10:42 +0530

    Add files via upload
    first commit

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to 1234! 👋")
+
+st.sidebar.success("Select a demo above.")
